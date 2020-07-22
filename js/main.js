3bc696dcc1457bf3d22cbab85a83c60a7ea209fb
Vishal Agrawal <vishalagrawalva22@gmail.com>
2020-07-22 10:13:13 +0530

    add basic operations(+, -, *, /)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("Hello!");
+function operate (num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1,num2);
+    } else if (operator === '-') {
+        return subtract (num1, num2);
+    } else if (operator === '*') {
+        return multiply (num1, num2);
+    } else if (operator === '/') {
+        return divide (num1, num2);
+    }
+}
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
